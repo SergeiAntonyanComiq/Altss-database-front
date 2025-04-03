76bcdaa1c43fd69689d4d596b502176bd8f606ef
Alex <alex@example.com>
2025-04-03 12:38:38 -0700

    Resolve merge conflict in PersonsListFooter.tsx

--- src/components/personal/list/PersonsListFooter.tsx
+++ src/components/personal/list/PersonsListFooter.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> a90c02c6fb60eed6ce832988ccc4464c37ce0f3d
 import React from "react";
 import PersonsPagination from "../PersonsPagination";
 
@@ -25,11 +21,7 @@ const PersonsListFooter: React.FC<PersonsListFooterProps> = ({
   disablePagination = false
 }) => {
   return (
-<<<<<<< HEAD
-    <div className="flex w-full gap-[40px_100px] justify-start flex-wrap mt-8">
-=======
     <div className="flex w-full gap-[40px_100px] justify-between flex-wrap mt-8">
->>>>>>> a90c02c6fb60eed6ce832988ccc4464c37ce0f3d
       <PersonsPagination 
         currentPage={currentPage}
         onPageChange={onPageChange}
