4c95fe6a96b1fd8804ec5113722e7c7ece4a4da2
SergeiAntonyanComiq <sergeicomiqai@gmail.com>
2025-05-08 19:00:39 +0400

    fix: Vercel configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // Ensure base path is set
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: [
-      "altss.a.pinggy.link",
-      "*.pinggy.link"
-    ],
+    allowedHosts: ["altss.a.pinggy.link", "*.pinggy.link"],
   },
-  plugins: [
-    react(),
-    // mode === 'development' &&
-    // componentTagger(),
-  ].filter(Boolean),
+  build: {
+    outDir: "dist",
+  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
