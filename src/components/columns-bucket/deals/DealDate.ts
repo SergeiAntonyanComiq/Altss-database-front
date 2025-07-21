c8310e1414ed0d286bd0767f6a0c77df830b1bb6
SergeiAntonyanComiq <sergeicomiqai@gmail.com>
2025-07-21 18:42:26 +0400

    fix: Deal page empty blank

--- src/components/columns-bucket/deals/DealDate.ts
+++ src/components/columns-bucket/deals/DealDate.ts
@@ -1,10 +1,14 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Deals } from "@/services/familyOfficesService.ts";
 import { format } from "date-fns";
+import { EMPTY_PLACEHOLDER } from "@/utils.tsx";
 
 export const DealDate: ColumnDef<Deals> = {
   id: "investment_date",
-  accessorFn: (row) => format(row.investment_date, "dd.MM.yyyy"),
+  accessorFn: (row) =>
+    row.investment_date
+      ? format(row.investment_date, "dd.MM.yyyy")
+      : EMPTY_PLACEHOLDER,
   meta: {
     maxWidth: 160,
     cellClassName:
